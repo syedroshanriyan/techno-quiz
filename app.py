af7837e054462ba5012a131f2ed7d816d7541908
syedroshanriyan <224979009+syedroshanriyan@users.noreply.github.com>
2026-04-23 22:13:43 +0530

    app.py

--- app.py
+++ app.py
@@ -133,4 +133,4 @@ def reset():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
